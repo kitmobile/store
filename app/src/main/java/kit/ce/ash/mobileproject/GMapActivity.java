d623cb50a3f46a7701492c5262dc0e9416de8ded
곽호성 <kakosung@gwaghoseongui-MacBook-Air.local>
2016-06-10 20:07:54 +0900

    내 위치 찾기 버튼 추가

--- app/src/main/java/kit/ce/ash/mobileproject/GMapActivity.java
+++ app/src/main/java/kit/ce/ash/mobileproject/GMapActivity.java
@@ -1,8 +1,12 @@
 package kit.ce.ash.mobileproject;
 
+import android.*;
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
+import android.support.v4.content.ContextCompat;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
@@ -83,5 +87,13 @@ public class GMapActivity extends FragmentActivity implements OnMapReadyCallback
                 finish();
             }
         });
+
+        // 현재 위치 찾기 버튼 추가
+        if (ContextCompat.checkSelfPermission(this, android.Manifest.permission.ACCESS_FINE_LOCATION)
+                == PackageManager.PERMISSION_GRANTED) {
+            mMap.setMyLocationEnabled(true);
+        } else {
+            // Show rationale and request permission.
+        }
     }
 }
